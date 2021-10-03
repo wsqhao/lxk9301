2a7f0971502d7a43f9895aa436fde36b7265d9b3
wsqhao <35697616+wsqhao@users.noreply.github.com>
2021-10-03 23:53:46 +0800

    Add files via upload

---
+++ yilai.sh
@@ -0,0 +1,12 @@
+npm install -g cnpm -registry=https://registry.npm.taobao.org
+cnpm install -g png-js
+cnpm install -g date-fns
+cnpm install -g axios
+cnpm install -g crypto-js
+cnpm install -g ts-md5
+cnpm install -g tslib
+cnpm install -g @types/node
+cnpm install -g requests
+cnpm install -g got
+cnpm install -g tunnel
+cnpm install -g jsdom
